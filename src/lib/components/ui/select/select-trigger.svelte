e25dce678a1cb330f4ea7665464936ae4f726765
HeisenBerg? <admin@tiktokastudio.com>
2025-05-09 20:38:13 +0800

    Update select-trigger.svelte

--- src/lib/components/ui/select/select-trigger.svelte
+++ src/lib/components/ui/select/select-trigger.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Select as SelectPrimitive } from 'bits-ui';
-	import CaretSort from '~icons/lucide/caret-sort';
+	import CaretSort from '~icons/lucide/caret-sort.svelte';
 	import { cn } from '$lib/utils.js';
 
 	type $$Props = SelectPrimitive.TriggerProps;
